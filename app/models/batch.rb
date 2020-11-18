0b8619553947abe431cfbb6014fcde192515031c
Gustavo Castro <lgx.0097@gmail.com>
2020-11-18 16:55:09 -0300

    ✅ Add validations

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,5 @@
 class Batch < ApplicationRecord
   has_many :orders
+
+  validates :reference, :purchase_channel, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :batch
+
+  validates :reference, :purchase_channel, :client_name, :address,
+            :delivery_service, :line_items, :status, presence: true
+
+  monetize :total_value_cents,
+    numericality: {
+      greater_than_or_equal_to: 0
+    }
+
   serialize :line_items, Array
-  monetize :total_value_cents
+
   enum status: %i[ready production closing sent]
 end
