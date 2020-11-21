2216bef0f77cdf27569eec9df34f74bbee21f640
Gustavo Castro <lgx.0097@gmail.com>
2020-11-21 16:36:33 -0300

    ✅ Add bonus action

--- app/controllers/api/v1/batches/shipments_controller.rb
+++ app/controllers/api/v1/batches/shipments_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::Batches::ShipmentsController < ApplicationController
   def update
-    @batch = Batch.joins(:orders).find_by(reference: params[:reference])
-    @batch.orders.update_all(status: 'sent')
+    reference = params[:reference]
+    delivery_service = params[:delivery_service]
 
+    @batch = Batch.joins(:orders).find_by(reference: params[:reference])
+    if delivery_service
+      @batch.orders.where(delivery_service: delivery_service).update_all(status: 'sent')
+    elsif reference
+      @batch.orders.update_all(status: 'sent').update_all(status: 'sent')
+    end
     render json: { reference: @batch.reference, status: 'sent' }, status: :ok
   rescue ActiveRecord::RecordNotFound
     head 404
