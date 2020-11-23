9040cd4a25ccdebe68a83c580eba446592210f86
Gustavo Castro <lgx.0097@gmail.com>
2020-11-23 00:18:43 -0300

    🐛 Fix shipments controller logic

--- app/controllers/api/v1/batches/shipments_controller.rb
+++ app/controllers/api/v1/batches/shipments_controller.rb
@@ -1,26 +1,18 @@
 class Api::V1::Batches::ShipmentsController < ApplicationController
   def update
     reference = params[:reference]
-    delivery_service = params[:delivery_service]
+    delivery_service = params[:options][:delivery_service] if params[:options]
 
     batch = Batch.joins(:orders).find_by(reference: reference)
     raise ActiveRecord::RecordNotFound if batch.nil?
 
-    orders = set_orders_to_sent(batch, delivery_service)
+    orders = batch.orders.where(delivery_service: delivery_service)
+    raise ActiveRecord::RecordNotFound if orders.nil? || orders.empty?
+
     orders.update_all(status: 'sent')
 
-    render json: { reference: batch.reference, status: 'sent' }, status: :ok
+    render json: { reference: batch.reference, status: 'sent', delivery_service: delivery_service }, status: :ok
   rescue ActiveRecord::RecordNotFound
     head 404
-  end
-
-  private
-
-  def set_orders_to_sent(batch, delivery_service)
-    if delivery_service
-      batch.orders.where(delivery_service: delivery_service)
-    else
-      batch.orders
-    end
   end
 end
