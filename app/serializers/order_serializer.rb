476b9b94ec3c4fbb008c7efe3cc4268ded495d02
Gustavo Castro <lgx.0097@gmail.com>
2020-11-23 19:16:27 -0300

    ✅ Add missing field on order serializar

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,7 +1,7 @@
 class OrderSerializer
   include JSONAPI::Serializer
   attributes :reference, :purchase_channel, :client_name, :address,
-             :delivery_service, :line_items, :status
+             :delivery_service, :line_items, :total_value_cents, :status
 
   belongs_to :batch
 end
